d578104fac39335e21fe475a56b774acf6b540de
gkoroniotis <koroniotis_g@hotmail.com>
2022-03-20 22:18:52 +0200

    Complete from task 2 subtasks 1-3

--- src/agileActorsSparkProject/gkor_task_02.py
+++ src/agileActorsSparkProject/gkor_task_02.py
@@ -6,140 +6,81 @@ from pyspark.sql import functions as f
 
 spark=SparkSession.builder.appName("AgileActorsSparkTeam").getOrCreate()
 
-
+# Paths
 orderItems_input_path = r"C:\Users\user\Documents\AgileActorsSparkTeams\AgileActorsSparkTeam\input_files\orderItemsParts_nested.json"
 orderItems_output_path = r"C:\Users\user\Documents\AgileActorsSparkTeams\AgileActorsSparkTeam\output\OrderItemParts"
 
-
-orderItems_schema = StructType([StructField("CLERK",StringType(),True),
-                                StructField("COMMENT",StringType(),True),
-                                StructField("CUSTKEY",IntegerType(),True),
-                                StructField("LINEITEMS",StructType([
-                                    StructField("COMMENT",StringType(),True),
-                                    StructField("COMMITDATE",StringType(),True),
-                                    StructField("DISCOUNT",DoubleType(),True),
-                                    StructField("EXTENTEDPRICE",DoubleType(),True),
-                                    StructField("LINENUMBER",IntegerType(),True),
-                                    StructField("LINESTATUS",StringType(),True),
-                                    StructField("ORDERKEY",IntegerType(),True),
-                                    StructField("PARTS",StructType([
-                                        StructField("BRAND",StringType(),True),
-                                        StructField("COMMENT",StringType(),True),
-                                        StructField("CONTAINER",StringType(),True),
-                                        StructField("MFGR",StringType(),True),
-                                        StructField("NAME",StringType(),True),
-                                        StructField("PARTKEY",IntegerType(),True),
-                                        StructField("RETAILPRICE",DoubleType(),True),
-                                        StructField("SIZE",LongType(),True),
-                                        StructField("TYPE",DoubleType(),True)
-                                        ]),True),
-                                    StructField("QUANTITY",DoubleType(),True),
-                                    StructField("RECEIPTDATE",StringType(),True),
-                                    StructField("RETURNFLAG",StringType(),True),
-                                    StructField("SHIPDATE",StringType(),True),
-                                    StructField("SHIPINSTRUCT",StringType(),True),
-                                    StructField("SHIPMODE",StringType(),True),
-                                    StructField("SUPPKEY",LongType(),True),
-                                    StructField("TAX",DoubleType(),True),
-                                    ]),True),
-                                StructField("ORDERDATE",StringType(),True),
-                                StructField("ORDERKEY",LongType(),True),
-                                StructField("ORDERPRIORITY",StringType(),True),
-                                StructField("ORDERSTATUS",StringType(),True),
-                                StructField("SHIPPRIORITY",LongType(),True),
-                                StructField("TOTALPRICE",DoubleType(),True)
-                            ])
-
-
-orderItems_df = spark.read.json(orderItems_input_path)
-               #.schema(orderItems_schema)\
-               
+# Read JSON file
+orderItems_df = spark.read.json(orderItems_input_path)              
 orderItems_df.printSchema()
 orderItems_df.show(5)
 
-
-orders_schema = StructType([
-    StructField("ORDERKEY",IntegerType(),True),
-    StructField("CUSTKEY",IntegerType(),True),
-    StructField("ORDERSTATUS",StringType(),True),
-    StructField("TOTALPRICE",DoubleType(),True),
-    StructField("ORDERDATE",StringType(),True),
-    StructField("ORDER-PRIORITY",StringType(),True),
-    StructField("CLERK",StringType(),True),
-    StructField("SHIP-PRIORITY",IntegerType(),True),
-    StructField("COMMENT",StringType(),True),
-])
-
-line_item_schema = StructType([
-    StructField("ORDERKEY",IntegerType(),True),
-    StructField("PARTKEY",IntegerType(),True),
-    StructField("SUPPKEY",IntegerType(),True),
-    StructField("LINENUMBER",IntegerType(),True),
-    StructField("QUANTITY",IntegerType(),True),
-    StructField("EXTENTEDPRICE",DoubleType(),True),
-    StructField("DISCOUNT",DoubleType(),True),
-    StructField("TAX",IntegerType(),True),
-    StructField("RETURNFLAG",IntegerType(),True),
-    StructField("LINESTATUS",StringType(),True),
-    StructField("SHIPDATE",IntegerType(),True),
-    StructField("COMMITDATE",StringType(),True),
-    StructField("RECEIPTDATE",IntegerType(),True),
-    StructField("SHIPINSTRUCT",IntegerType(),True),
-    StructField("SHIPMODE",IntegerType(),True),
-    StructField("COMMENT",StringType(),True),
-    ])
-
-part_schema = StructType([
-    StructField("PARTKEY",IntegerType(),True),
-    StructField("NAME",IntegerType(),True),
-    StructField("MFGR",IntegerType(),True),
-    StructField("BRAND",IntegerType(),True),
-    StructField("TYPE",IntegerType(),True),
-    StructField("SIZE",IntegerType(),True),
-    StructField("CONTAINER",IntegerType(),True),
-    StructField("RETAILPRICE",IntegerType(),True),
-    StructField("COMMENT",IntegerType(),True),
-    ])
-
-#order_df = orderItems_df.select("o_orderkey","o_custkey","o_orderstatus","o_totalprice","o_orderdate", "o_orderpriority", "o_clerk", "o_shippriority", "o_comment")
-# line_item_df = orderItems_df.select(f.array(f.expr("o_lineitems.*")))
-# line_item_df = orderItems_df.select("o_lineitmes.element.l_orderkey"
-#                                   , "o_lineitmes.element.l_parts.element.p_partkey"
-#                                   , "o_lineitmes.element.l_suppkey"
-#                                   , "o_lineitmes.element.l_linenumber"
-#                                   , "o_lineitmes.element.l_quantity"
-#                                   , "o_lineitmes.element.l_extendedprice"
-#                                   , "o_lineitmes.element.l_discount"
-#                                   , "o_lineitmes.element.l_tax"
-#                                   , "o_lineitmes.element.l_returnflag"
-#                                   , "o_lineitmes.element.l_linestatus"
-#                                   , "o_lineitmes.element.l_shipdate"
-#                                   , "o_lineitmes.element.l_commitdate"
-#                                   , "o_lineitmes.element.l_receiptdate"
-#                                   , "o_lineitmes.element.l_shipinstruct"
-#                                   , "o_lineitmes.element.l_shipmode"
-#                                   , "o_lineitmes.element.l_comment"
-#                                   )
-# part_df = orderItems_df.select("o_lineitems.element.l_parts.element.p_partkey", 
-#                                 "o_lineitems.element.l_parts.element.p_name", 
-#                                 "o_lineitems.element.l_parts.element.p_mfgr", 
-#                                 "o_lineitems.element.l_parts.element.p_brand:", 
-#                                 "o_lineitems.element.l_parts.element.p_type", 
-#                                 "o_lineitems.element.l_parts.element.p_size", 
-#                                 "o_lineitems.element.l_parts.element.p_container", 
-#                                 "o_lineitems.element.l_parts.element.p_retailprice", 
-#                                 "o_lineitems.element.l_parts.element.p_comment"
-#                               )                              
-
-#order_df.show(5)
-#lineItems_df = orderItems_df.select(orderItems_df.o_orderkey, f.explode(orderItems_df.o_lineitems).alias('lineItems'))
-lineItems_df = orderItems_df.withColumn("o_lineitems", f.explode(f.col("o_lineitems"))).select("o_lineitems.*")
+# ORDER TABLE - Select only the columns I want in order to create a 3NF table
+order_df = orderItems_df.select("o_orderkey","o_custkey","o_orderstatus","o_totalprice","o_orderdate", "o_orderpriority", "o_clerk", "o_shippriority", "o_comment")
+                     
+# LINEITMES TABLE - Select only the columns I want in order to create a 3NF table                      
+lineItems_df = orderItems_df.withColumn("o_lineitems", f.explode(f.col("o_lineitems"))).select("o_lineitems.l_orderkey"                                                                                              
+                                                                                              ,"o_lineitems.l_suppkey"
+                                                                                              ,"o_lineitems.l_linenumber"
+                                                                                              ,"o_lineitems.l_quantity"
+                                                                                              ,"o_lineitems.l_extendedprice"
+                                                                                              ,"o_lineitems.l_discount"
+                                                                                              ,"o_lineitems.l_tax"
+                                                                                              ,"o_lineitems.l_returnflag"
+                                                                                              ,"o_lineitems.l_linestatus"
+                                                                                              ,"o_lineitems.l_shipdate"
+                                                                                              ,"o_lineitems.l_commitdate"
+                                                                                              ,"o_lineitems.l_receiptdate"
+                                                                                              ,"o_lineitems.l_shipinstruct"
+                                                                                              ,"o_lineitems.l_shipmode"
+                                                                                              ,"o_lineitems.l_comment"
+                                                                                              ,"o_lineitems.l_parts"
+                                                                                              )\
+                            .withColumn("l_parts",f.explode(f.col("l_parts"))).select("l_orderkey"
+                                                                                     , f.col("l_parts.p_partkey").alias('l_partkey')
+                                                                                     ,"l_suppkey"
+                                                                                     ,"l_linenumber"
+                                                                                     ,"l_quantity"
+                                                                                     ,"l_extendedprice"
+                                                                                     ,"l_discount"
+                                                                                     ,"l_tax"
+                                                                                     ,"l_returnflag"
+                                                                                     ,"l_linestatus"
+                                                                                     ,"l_shipdate"
+                                                                                     ,"l_commitdate"
+                                                                                     ,"l_receiptdate"
+                                                                                     ,"l_shipinstruct"
+                                                                                     ,"l_shipmode"
+                                                                                     ,"l_comment"
+                                                                                     ,"l_parts", )
 lineItems_df.printSchema()
-lineItems_df.show(5)
-#part_df.show(5)
 
-
-parts_df = lineItems_df.withColumn("l_parts", f.explode(f.col("l_parts"))).select("l_parts.*")
+# PARTS TABLE - Select only the columns I want in order to create a 3NF table                      
+parts_df = lineItems_df.withColumn("l_parts", f.col("l_parts")).select("l_parts.p_partkey"
+                                                                      ,"l_parts.p_name"
+                                                                      ,"l_parts.p_mfgr"
+                                                                      ,"l_parts.p_brand"
+                                                                      ,"l_parts.p_type"
+                                                                      ,"l_parts.p_size"
+                                                                      ,"l_parts.p_container"
+                                                                      ,"l_parts.p_retailprice"
+                                                                      ,"l_parts.p_comment"
+                                                                                 )
 parts_df.printSchema()
-parts_df.show(5)
+#parts_df.show(5)
 
+# Drop unnecessary fields
+lineItems_df = lineItems_df.drop("l_parts")
+
+# Show my tables
+order_df.show(5, truncate=False)
+lineItems_df.show(5,truncate=False)
+parts_df.show(5,truncate=False)
+
+# write to parquet the 3 tables schemas
+order_df.write.mode("overwrite").parquet(orderItems_output_path+r'\orders')
+lineItems_df.write.mode("overwrite").parquet(orderItems_output_path+r'\lineItems')
+parts_df.write.mode("overwrite").parquet(orderItems_output_path+r'\parts')
+
+# write the initial json file to parquet file
+orderItems_df.write.mode("overwrite").parquet(orderItems_output_path+r'\orderItemsJSON')
